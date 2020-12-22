f3959fa144135f731d5d58c20c39d3a0560e992f
flodorner <38560862+flodorner@users.noreply.github.com>
2020-12-22 19:50:14 +0100

    Added conditional policy based on the last variable to td3

--- spinup/algos/pytorch/td3/core.py
+++ spinup/algos/pytorch/td3/core.py
@@ -17,15 +17,40 @@ def mlp(sizes, activation, output_activation=nn.Identity):
         layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
     return nn.Sequential(*layers)
 
+class split_model(nn.Module):
+    def __init__(self,sizes, activation, output_activation=nn.Identity):
+        super(split_model, self).__init__()
+        self.layers = len(sizes) - 1
+        self.layers_a = []
+        for j in range(len(sizes) - 1):
+            act = activation if j < len(sizes) - 2 else output_activation
+            self.layers_a += [nn.Linear(sizes[j], sizes[j + 1]), act()]
+        self.layers_b = []
+        for j in range(len(sizes) - 1):
+            act = activation if j < len(sizes) - 2 else output_activation
+            self.layers_b += [nn.Linear(sizes[j], sizes[j + 1]), act()]
+    def forward(self, x):
+        split = torch.eq(x[:,-1],1).unsqueeze(1)
+        for j in range(self.layers):
+            x = split*self.layers_a(x)+(1-split)*self.layers_b(x)
+        return x
+
+def mlp_last_variable_switch(sizes, activation, output_activation=nn.Identity):
+    return split_model(sizes, activation, output_activation)
+
+
 def count_vars(module):
     return sum([np.prod(p.shape) for p in module.parameters()])
 
 class MLPActor(nn.Module):
 
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation, act_limit):
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation, act_limit,use_split=False):
         super().__init__()
         pi_sizes = [obs_dim] + list(hidden_sizes) + [act_dim]
-        self.pi = mlp(pi_sizes, activation, nn.Tanh)
+        if not use_split:
+            self.pi = mlp(pi_sizes, activation, nn.Tanh)
+        else:
+            self.pi = mlp_last_variable_switch(pi_sizes, activation, nn.Tanh)
         self.act_limit = act_limit
 
     def forward(self, obs):
@@ -34,9 +59,12 @@ class MLPActor(nn.Module):
 
 class MLPQFunction(nn.Module):
 
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation,use_split=False):
         super().__init__()
-        self.q = mlp([obs_dim + act_dim] + list(hidden_sizes) + [1], activation)
+        if not use_split:
+            self.q = mlp([obs_dim + act_dim] + list(hidden_sizes) + [1], activation)
+        else:
+            self.q = mlp_last_variable_switch([obs_dim + act_dim] + list(hidden_sizes) + [1], activation)
 
     def forward(self, obs, act):
         q = self.q(torch.cat([obs, act], dim=-1))
@@ -45,7 +73,7 @@ class MLPQFunction(nn.Module):
 class MLPActorCritic(nn.Module):
 
     def __init__(self, observation_space, action_space, hidden_sizes=(256,256),
-                 activation=nn.ReLU):
+                 activation=nn.ReLU,use_split=False):
         super().__init__()
 
         obs_dim = observation_space.shape[0]
@@ -53,9 +81,9 @@ class MLPActorCritic(nn.Module):
         act_limit = action_space.high[0]
 
         # build policy and value functions
-        self.pi = MLPActor(obs_dim, act_dim, hidden_sizes, activation, act_limit)
-        self.q1 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation)
-        self.q2 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation)
+        self.pi = MLPActor(obs_dim, act_dim, hidden_sizes, activation, act_limit,use_split=use_split)
+        self.q1 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation,use_split=use_split)
+        self.q2 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation,use_split=use_split)
 
     def act(self, obs):
         with torch.no_grad():
