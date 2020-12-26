48951ceaa733018de95c971bb682872392139a3f
flodorner <38560862+flodorner@users.noreply.github.com>
2020-12-26 14:32:14 +0100

    Added conditional sac

--- spinup/algos/pytorch/sac/core.py
+++ spinup/algos/pytorch/sac/core.py
@@ -19,6 +19,23 @@ def mlp(sizes, activation, output_activation=nn.Identity):
         layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
     return nn.Sequential(*layers)
 
+class split_model(nn.Module):
+    def __init__(self,sizes, activation, output_activation=nn.Identity,split_index=-1):
+        super(split_model, self).__init__()
+        self.mlp1 = mlp(sizes, activation, output_activation)
+        self.mlp2 = mlp(sizes, activation, output_activation)
+        self.split_index = split_index
+    def forward(self, x):
+        if len(list(x.size()))==1:
+            x = x.unsqueeze(0)
+        split = torch.eq(x[:,self.split_index],1).unsqueeze(1)
+        x = split.float()*self.mlp1(x)+torch.logical_not(split).float()*self.mlp2(x)
+        return x
+
+def mlp_switch(sizes, activation, output_activation=nn.Identity,split_index=-1):
+    return split_model(sizes, activation, output_activation,split_index=split_index)
+
+
 def count_vars(module):
     return sum([np.prod(p.shape) for p in module.parameters()])
 
@@ -28,14 +45,19 @@ LOG_STD_MIN = -20
 
 class SquashedGaussianMLPActor(nn.Module):
 
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation, act_limit):
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation, act_limit,use_split=False):
         super().__init__()
-        self.net = mlp([obs_dim] + list(hidden_sizes), activation, activation)
+        if not use_split:
+            self.net = mlp([obs_dim] + list(hidden_sizes), activation, activation)
+        else:
+            self.net = mlp_switch([obs_dim] + list(hidden_sizes), activation, activation)
         self.mu_layer = nn.Linear(hidden_sizes[-1], act_dim)
         self.log_std_layer = nn.Linear(hidden_sizes[-1], act_dim)
         self.act_limit = act_limit
 
     def forward(self, obs, deterministic=False, with_logprob=True):
+        #Unclear if this is solved optimally. Maybe define a two-headed mlp instead (such that mu and log_std depend
+        # on the last index?!)
         net_out = self.net(obs)
         mu = self.mu_layer(net_out)
         log_std = self.log_std_layer(net_out)
@@ -69,9 +91,12 @@ class SquashedGaussianMLPActor(nn.Module):
 
 class MLPQFunction(nn.Module):
 
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation,use_split=False):
         super().__init__()
-        self.q = mlp([obs_dim + act_dim] + list(hidden_sizes) + [1], activation)
+        if not use_split:
+            self.q = mlp([obs_dim + act_dim] + list(hidden_sizes) + [1], activation)
+        else:
+            self.q = mlp_switch([obs_dim + act_dim] + list(hidden_sizes) + [1], activation,split_index=-1-act_dim)
 
     def forward(self, obs, act):
         q = self.q(torch.cat([obs, act], dim=-1))
@@ -91,6 +116,26 @@ class MLPActorCritic(nn.Module):
         self.pi = SquashedGaussianMLPActor(obs_dim, act_dim, hidden_sizes, activation, act_limit)
         self.q1 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation)
         self.q2 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation)
+
+    def act(self, obs, deterministic=False):
+        with torch.no_grad():
+            a, _ = self.pi(obs, deterministic, False)
+            return a.numpy()
+
+class MLPActorCriticSplit(nn.Module):
+
+    def __init__(self, observation_space, action_space, hidden_sizes=(256,256),
+                 activation=nn.ReLU):
+        super().__init__()
+
+        obs_dim = observation_space.shape[0]
+        act_dim = action_space.shape[0]
+        act_limit = action_space.high[0]
+
+        # build policy and value functions
+        self.pi = SquashedGaussianMLPActor(obs_dim, act_dim, hidden_sizes, activation, act_limit,use_split=True)
+        self.q1 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation,use_split=True)
+        self.q2 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation,use_split=True)
 
     def act(self, obs, deterministic=False):
         with torch.no_grad():
