928fe2517c266f0cfe99257106b99e714f5b17d4
flodorner <floriandorner@gmx.de>
2021-01-24 20:15:52 +0100

    Modified the wrapper to use a "safety critic"

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import gym
 from wrapper import wrapper
 from spinup import td3_lagrange_pytorch
 
-env = gym.make('Safexp-PointGoal1-v0')
+env = wrapper(gym.make('Safexp-PointGoal1-v0'))
 
 td3_lagrange_pytorch(lambda: env, epochs=1000, steps_per_epoch=10000, start_steps=10000,
                      act_noise=0.0, ac_kwargs=dict(hidden_sizes=[256,256],hidden_sizes_policy=[256,256]),num_test_episodes=10,
