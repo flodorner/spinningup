96bf9eb7a9c42f2b3efad2cd6803f1910e034a33
flodorner <floriandorner@gmx.de>
2021-01-27 09:37:55 +0100

    Changed experiment

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from spinup import td3_lagrange_pytorch,sac_lagrange_pytorch
 
 env = constraint_wrapper(gym.make('Safexp-PointGoal1-v0'))
 
-td3_lagrange_pytorch(lambda: env, epochs=25, steps_per_epoch=10000, start_steps=10000,
+sac_lagrange_pytorch(lambda: env, epochs=25, steps_per_epoch=10000, start_steps=10000,
                      ac_kwargs=dict(hidden_sizes=[256,256],hidden_sizes_policy=[256,256]),num_test_episodes=0,
-                                    batch_size=100,data_aug=False,threshold=False,lambda_soft=-10000.0)
+                                    batch_size=100,data_aug=False,threshold=False,lambda_soft=-10000.0,gamma=0)
