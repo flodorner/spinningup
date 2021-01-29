26454f7f733df53d192092de40b758cd98b7807f
flodorner <floriandorner@gmx.de>
2021-01-29 17:59:12 +0100

    Added observation stacking

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ from spinup import td3_lagrange_pytorch,sac_lagrange_pytorch
 env = constraint_wrapper(gym.make('Safexp-PointGoal1-v0'),buckets=0,stack_obs=3)
 
 sac_lagrange_pytorch(lambda: env, epochs=10, steps_per_epoch=25000, start_steps=10000,
-                     ac_kwargs=dict(hidden_sizes=[256,256],hidden_sizes_policy=[256,256,256]),num_test_episodes=25,lr=1e-4,
+                     ac_kwargs=dict(hidden_sizes=[256,256,256],hidden_sizes_policy=[256,256]),num_test_episodes=25,lr=1e-4,
                                     batch_size=100,data_aug=False,threshold=False,lambda_soft=5,gamma=0.5)
